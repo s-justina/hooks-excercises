a72872ef988ce096e8440d97972c2d545ecd31db
s-justina <jus.sobcz@gmail.com>
2020-07-22 20:31:30 +0200

    functional component template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ClassComponentExample from "./components/ClassComponentExample";
+import HooksExample from "./components/HooksExample";
 
 function App() {
   return (
     <div className="App">
       <h2>Lets check how is it work!</h2>
       <ClassComponentExample/>
+      <HooksExample/>
     </div>
   );
 }
