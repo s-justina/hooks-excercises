0ec95dff67a69eb3f0e33b4e5e4e5b19264d8e79
s-justina <jus.sobcz@gmail.com>
2020-07-23 15:09:08 +0200

    check simple dependencies in functional component in React - Hook useMemo;

--- src/components/HooksExample.js
+++ src/components/HooksExample.js
@@ -1,24 +1,31 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
 import {getSingle} from "../utils";
 
 export default function HooksExample({id}){
     const [data, setData] = useState();
-    const [dommyData, setDommyData] = useState(0)
+    const [dommyData, setDommyData] = useState(0);
+
     useEffect(()=>{
-        console.log('functional component with hook: 1');
+        console.log('functional component with hook: 1 - useEffect');
         const data = getSingle(id);
         console.log(data);
         setData(data);
     }, [id]);
     useEffect(()=>{
-        console.log('functional component with hook: 2 - dommy data:');
+        console.log('functional component with hook: 2 - useEffect with dommy data:');
     },[dommyData]);
+    const memoData=useMemo(()=>{
+        console.log('functional component with hook: 3 - useMemo')
+        return getSingle(id);
+    }, [id]);
 
     return(
         <React.Fragment>
           <pre>
               HooksExample
               {JSON.stringify({data}, null, 2)}
+              <br/>
+              {JSON.stringify({memoData}, null, 2)}
           </pre>
             <button onClick={()=>setDommyData(dommyData+1)}>change dommy data</button>
         </React.Fragment>
