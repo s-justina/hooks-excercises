54d0dfd876cada538a1156def5accb912b76fa28
s-justina <jus.sobcz@gmail.com>
2020-07-23 16:35:45 +0200

    check simple dependencies in functional component in React - Hook useCallback;

--- src/components/HooksExample.js
+++ src/components/HooksExample.js
@@ -1,28 +1,32 @@
-import React, {useState, useEffect, useMemo} from 'react';
+import React, {useState, useEffect, useMemo, useCallback} from 'react';
 import {getSingle} from "../utils";
 import {useSingleHook} from "../utils";
 
-export default function HooksExample({id}){
+export default function HooksExample({id}) {
     const [data, setData] = useState();
     const [dommyData, setDommyData] = useState(0);
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('functional component with hook: 1 - useEffect');
         const data = getSingle(id);
         console.log(data);
         setData(data);
     }, [id]);
-    useEffect(()=>{
+    useEffect(() => {
         console.log('functional component with hook: 2 - useEffect with dommy data:');
-    },[dommyData]);
-    const memoData=useMemo(()=>{
-        console.log('functional component with hook: 3 - useMemo')
+    }, [dommyData]);
+    const memoData = useMemo(() => {
+        console.log('functional component with hook: 3 - useMemo');
         return getSingle(id);
     }, [id]);
 
     const hookData = useSingleHook(id);
 
-    return(
+    const handleClick = useCallback(()=>{
+        setDommyData(dommyData + 1)
+    }, [dommyData]);
+
+    return (
         <React.Fragment>
           <pre>
               HooksExample
@@ -32,7 +36,15 @@ export default function HooksExample({id}){
               <br/>
               {JSON.stringify({hookData}, null, 2)}
           </pre>
-            <button onClick={()=>setDommyData(dommyData+1)}>change dommy data</button>
+            <button onClick={() => setDommyData(dommyData + 1)}>change dommy data</button>
+            <Button onClick={handleClick}> New change dommy data</Button>
         </React.Fragment>
     )
 };
+
+const Button = React.memo(({onClick, children}) => {
+    console.log(onClick);
+    return (
+        <button onClick={onClick}>{children}</button>
+    )
+});
